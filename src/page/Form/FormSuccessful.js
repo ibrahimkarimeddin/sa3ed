7cdceaada8427ca9206edf33335e9f3e5ea1c0f4
ibrahim karim eddin <ibrahimkarimeddin@gmail.com>
2023-01-22 23:19:20 +0300

    HCANGE WIDTH

--- src/page/Form/FormSuccessful.js
+++ src/page/Form/FormSuccessful.js
@@ -7,7 +7,7 @@ function FormSuccessful() {
     <div className='w-full flex justify-center'>
         <img src='/logo/logo.svg' className='h-20' alt='logo'/>
     </div>
-    <p className='text-white font-bold bg-[#c3c3d7] w-[15vw] text-center my-7 p-2 rounded-[20px] animate-bounce mx-auto'>تم توصيل طلبك ب نجاح </p>
+    <p className='text-white font-bold bg-[#c3c3d7] w-[30vw] md:w-[15vw] text-center my-7 p-2 rounded-[20px] animate-bounce mx-auto'>تم توصيل طلبك ب نجاح </p>
 </div>
   )
 }
